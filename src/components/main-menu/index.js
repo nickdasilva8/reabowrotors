ff88f35e034df4264d6ddedb3839bcf9e81c8172
Nick Da Silva <NickDaSilva@ip-192-168-0-19.eu-west-1.compute.internal>
2018-01-01 20:54:15 +0000

    RR-flatten commits to remove netlify setup.

--- src/components/main-menu/index.js
+++ src/components/main-menu/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export default class MainMenu extends React.Component {
   render() {
     // builds all the items inside the <li>'s of menu
-    // doesn't handle 'active' tab yet
+    // doesn't handle 'active' tab yet.
     const menu = this.props.menuItems.map((menuItem, index) => {
       return (
         <li key={index} role="presentation">
