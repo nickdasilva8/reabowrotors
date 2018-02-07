175f9110a1f8cae2900a4b02a1ab120de09d26af
Nick Da Silva <NickDaSilva@IT-Admins-MacBook-Pro.local>
2018-02-07 21:37:49 +0000

    RR-holdingPage: completed temp page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,70 +1,77 @@
 import React from 'react';
 import BootstrapContainer from './../components/bootstrap-container';
-import { Col } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 
 const IndexPage = () => (
   <div>
     <BootstrapContainer className="first-box" backgroundImage={true} id="about">
-      <h1>Content? What content? This is the about section!</h1>
+      <h1>About ReabowRotors</h1>
+
       <p>
-        As you can see, I don't really have enough information to put on my
-        website. So I should spend an hour or two thinking about what kind of
-        information I want on my landing page.
+        Currently <strong>ReabowRotors</strong> is being developed. Should you
+        wish to contact me during this period, you can do so with the following
+        details:
       </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+      <ul>
+        <li>
+          <strong>Email Address: </strong>jared_reabow@outlook.com
+        </li>
+        <li>
+          <strong>Telephone Number:</strong> 07929596513
+        </li>
+      </ul>
     </BootstrapContainer>
     <BootstrapContainer
       className="second-box"
       backgroundImage={false}
       id="videos">
-      <Col sm={12} md={4}>
-        <h1>Videos</h1>
-        <p>Something something here are some videos, something something.</p>
-        <p>
-          See my YouTube channel for more videos.{' '}
-          <a href="https://www.youtube.com/c/jaredreabow">Click here</a>
-        </p>
-      </Col>
-      <Col sm={12} md={4}>
-        <h2>Drone chases car</h2>
-        <iframe
-          width="auto"
-          height="auto"
-          src="https://www.youtube.com/embed/EBTg6qX1WMk?rel=0&amp;showinfo=0"
-          frameBorder="0"
-          allowFullScreen
-        />
-      </Col>
-      <Col sm={12} md={4}>
-        <h2>A glorious sunset</h2>
-        <iframe
-          width="auto"
-          height="auto"
-          src="https://www.youtube.com/embed/skw1ERNbkls?rel=0&amp;showinfo=0&amp;start=15"
-          frameBorder="0"
-          allowFullScreen
-        />
-      </Col>
-    </BootstrapContainer>
-    <BootstrapContainer
-      className="third-box"
-      backgroundImage={false}
-      id="gallery">
-      The gallery Section
-    </BootstrapContainer>
-    <BootstrapContainer
-      className="third-box"
-      backgroundImage={false}
-      id="gallery">
-      The footer? This needs to be its own component.
+      <Row>
+        <Col md={12}>
+          <h1>Videos</h1>
+          <p>
+            See my YouTube channel for more videos.{' '}
+            <a href="https://www.youtube.com/c/jaredreabow">Click here</a>
+          </p>
+        </Col>
+      </Row>
+      <Row style={{ paddingTop: '40px' }}>
+        <Col xs={12} sm={6} md={4}>
+          <h2>
+            Welcome to{' '}
+            <strong style={{ fontStyle: 'italics' }}>ReabowRotors</strong>
+          </h2>
+          <iframe
+            width="auto"
+            height="auto"
+            src="https://www.youtube.com/embed/iyryA9kXeBA?rel=0&amp;showinfo=0"
+            frameBorder="0"
+            allowFullScreen
+            style={{ maxWidth: '100%' }}
+          />
+        </Col>
+        <Col xs={12} sm={6} md={4}>
+          <h2>Westage construction project</h2>
+          <iframe
+            width="auto"
+            height="auto"
+            src="https://www.youtube.com/embed/KigtkFDwk3g?rel=0&amp;showinfo=0&amp;start=125"
+            frameBorder="0"
+            allowFullScreen
+            style={{ maxWidth: '100%' }}
+          />
+        </Col>
+        <Col xs={12} sm={6} md={4}>
+          <h2>A glorious Oxfordshire sunset</h2>
+          <iframe
+            width="auto"
+            height="auto"
+            src="https://www.youtube.com/embed/skw1ERNbkls?rel=0&amp;showinfo=0&amp;start=15"
+            frameBorder="0"
+            allowFullScreen
+            style={{ maxWidth: '100%' }}
+          />
+        </Col>
+      </Row>
     </BootstrapContainer>
   </div>
 );
